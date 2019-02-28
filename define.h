35847d6d9b6f9e98285ceccf363f5255bdc12ea7
Alkassoum_Yacine <yacine.alkassoum.etu@univ-lemans.fr>
2019-02-28 11:46:47 +0100

    ajout de fichiers supplementaires , headers...

---
+++ define.h
@@ -0,0 +1,6 @@
+#if !defined(N)
+#define N 8
+#define VIDE ' '
+#define NOIR 'X'  /* joueur 1 */
+#define BLANC 'O' /* joueur 2 */
+#endif 
---
+++ matrice.h
@@ -0,0 +1,11 @@
+#ifndef __MATRICE_H__
+#define __MATRICE_H__
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+#endif
