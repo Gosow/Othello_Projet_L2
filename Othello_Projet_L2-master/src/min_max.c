a34c83c22cb2b43710e19df2467263173cf79f40
Alkassoum_Yacine <yacine.alkassoum.etu@univ-lemans.fr>
2019-03-02 19:42:05 +0100

    ajout du min max pour le jeux contre l'ordinateur

---
+++ Othello_Projet_L2-master/src/min_max.c
@@ -0,0 +1,18 @@
+#include "define.h"
+
+
+
+
+
+//fonction qui retourne le nombre de points par rapport a un coup, plus le return est grand et plus le coup est interressant
+
+
+int point(t_matrice m , int color)
+{
+    int i,j;//compteurs lignes/colonnes
+    int cpt,calc;//compteurs de points
+    int b_d=0, h_d=0,b_g=0,h_g=0;//haut gauche/droit , bas gauche/droit
+
+
+    
+}
