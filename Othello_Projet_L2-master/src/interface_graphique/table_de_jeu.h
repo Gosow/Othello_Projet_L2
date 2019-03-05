a436847952ec366ea63d7619ca30340e98df8c3a
Mario <mariohotaj@gmail.com>
2019-03-05 18:56:20 +0100

    Separation des fonctions en plusieurs fichiers

---
+++ Othello_Projet_L2-master/src/interface_graphique/table_de_jeu.h
@@ -0,0 +1,13 @@
+//
+//  table_de_jeu.h
+//  
+//
+//  Created by Mario Hotaj on 05/03/2019.
+//
+
+#ifndef table_de_jeu_h
+#define table_de_jeu_h
+
+#include <stdio.h>
+
+#endif /* table_de_jeu_h */
