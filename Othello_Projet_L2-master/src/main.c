d8478fb7c98f760d3794e91819ab90b4012b1d5d
chaaab <48021425+chaaab@users.noreply.github.com>
2019-03-05 15:50:36 +0100

    Ajout fonction calculer_score dans le main

--- Othello_Projet_L2-master/src/main.c
+++ Othello_Projet_L2-master/src/main.c
@@ -11,6 +11,7 @@ int main (int argc,char **argv) {
     t_matrice m;
     int lig, col, joueur = 1;
     int choix;
+    int score1=0,score2=0;
 
 //Initialisation du jeux
     init_matrice (m);
@@ -18,7 +19,7 @@ int main (int argc,char **argv) {
     do{
         /* Affichage du menu */
 		printf("\nMenu :\n");
-		printf(" 1 - Jouer contre Un ami sur le même ordi\n");
+		printf(" 1 - Jouer contre Un ami sur le mÃªme ordi\n");
 		printf(" 2 - Jouer contre l'ordinateur\n");
 		printf(" 3 - Jouer en reseaux contre un ami\n");
         printf(" 4 - Quitter\n");
@@ -39,7 +40,12 @@ int main (int argc,char **argv) {
             if (peut_jouer(m, joueur_suivant(joueur)))
             joueur = joueur_suivant (joueur);
             else printf ("\nLe joueur %d passe son tour\n", joueur_suivant(joueur));
+            calculer_score(m,&score1,&score2);
+            printf("il y a %d pions du joueur 1 \n et %d du joueur 2 \n",score1,score2);
+            
+
          }
+
             break;
         case 2: // min max ordi avec fonction
         break;
