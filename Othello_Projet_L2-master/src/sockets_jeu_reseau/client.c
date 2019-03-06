a588ed23e076c7c68c8094c330a4dd54ad3473bd
Fatnassi mendy <fatmendy72@hotmail.fr>
2019-03-06 15:01:45 +0100

    ajout commentaire code

--- Othello_Projet_L2-master/src/sockets_jeu_reseau/client.c
+++ Othello_Projet_L2-master/src/sockets_jeu_reseau/client.c
@@ -54,6 +54,7 @@ void quitter(int to_server_socket){
 
 int main (  int argc, char** argv )
 {
+	//struct socka_addr permet de configurer la connexion (contexte d'addressage)
 	struct sockaddr_in serveur_addr;
 	struct hostent *serveur_info;
 	long hostAddr;
@@ -72,14 +73,19 @@ int main (  int argc, char** argv )
 	  	}
 	  	bcopy(serveur_info->h_addr,&serveur_addr.sin_addr,serveur_info->h_length);
 	}
+	//sin_port sera égal à la valeur retournée par la fonction htons, avec comme paramètre le port utilisé
 	serveur_addr.sin_port = htons(30000);
 	serveur_addr.sin_family = AF_INET;
 	/* creation de la socket */
+	//int socket(int domain, int type, int protocol) 
+	//avec domain:AF_INET pour TCP/IP et type :SOCK_STREAM si on utilise TCP/IP
 	if ( (to_server_socket = socket(AF_INET,SOCK_STREAM,0)) < 0) {
 		printf("Impossible de créer la socket client\n");
 	  	exit(0);
 	}
 	/* requete de connexion */
+	//int connect(int socket, struct sockaddr* addr, socklen_t addrlen);
+	//avec sockaddr* :  pointeur sur le contexte d'adressage du client et socklen : taille du contexte d'adressage comme le fonction accept() cote serveur
 	if(connect( to_server_socket, (struct sockaddr *)&serveur_addr, sizeof(serveur_addr)) < 0 ) {
 		printf("Impossible de se connecter au serveur\n");
 	  	exit(0);
--- Othello_Projet_L2-master/src/sockets_jeu_reseau/serveur.c
+++ Othello_Projet_L2-master/src/sockets_jeu_reseau/serveur.c
@@ -8,6 +8,8 @@
 #include<netdb.h>
 #include <signal.h>
 #include <unistd.h>
+
+#define SOCKET_ERROR -1
 char buffer[512];
 
 void fin(int sig){
@@ -55,11 +57,15 @@ int main ( void )
 {
 	int ma_socket;
 	int client_socket;
+	int sock_err;
+	//struct socka_addr permet de configurer la connexion (contexte d'addressage)
 	struct sockaddr_in mon_address, client_address;
 	unsigned int mon_address_longueur, lg;
 	bzero(&mon_address,sizeof(mon_address));
+	//sin_port sera égal à la valeur retournée par la fonction htons, avec comme paramètre le port utilisé
 	mon_address.sin_port = htons(30000);
 	mon_address.sin_family = AF_INET;
+	//sin_addr.s_addr sera l'IP donnée automatiquement au serveur. Pour le connaître nous utiliserons la fonction htonl avec comme seul paramètre la valeur INADDR_ANY
 	mon_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	char *hostname = "localhost";
@@ -70,23 +76,35 @@ int main ( void )
 	view_ip();
 
 	/* creation de socket */
-	if ((ma_socket = socket(AF_INET,SOCK_STREAM,0))== -1) {
+	//int socket(int domain, int type, int protocol) 
+	//avec domain:AF_INET pour TCP/IP et type :SOCK_STREAM si on utilise TCP/IP
+	if ((ma_socket = socket(AF_INET,SOCK_STREAM,0))== SOCKET_ERROR) {
 		printf("Impossible de créer la socket\n");
 		exit(0);
 	}
 
 	/* bind serveur - socket */
-	bind(ma_socket,(struct sockaddr *)&mon_address,sizeof(mon_address));
-	/* ecoute sur la socket */
-	listen(ma_socket,5);
-	/* accept la connexion */
-	mon_address_longueur = sizeof(client_address);
-	
-    /* on attend que le client se connecte */
-	client_socket = accept(ma_socket,
-                         (struct sockaddr *)&client_address,
-                         &mon_address_longueur);
+	//bind permet d'etablir la connexion avec le client
+	sock_err=bind(ma_socket,(struct sockaddr *)&mon_address,sizeof(mon_address));
+
+	if(sock_err != SOCKET_ERROR){
+		/* ecoute sur la socket */
+		sock_err=listen(ma_socket,5);
+
+		if(sock_err != SOCKET_ERROR){
+			/* accept la connexion */
+			mon_address_longueur = sizeof(client_address);
+			
+		    /* on attend que le client se connecte */
+		    //int accept(int socket, struct sockaddr* addr, socklen_t* addrlen);
+		    //avec sockaddr* :  pointeur sur le contexte d'adressage du client et socklen : taille du contexte d'adressage
+			client_socket = accept(ma_socket,
+	                         (struct sockaddr *)&client_address,
+	                         &mon_address_longueur);
+		}
     
+	}
+	
 	int quitter=0;
     while(!quitter)
 	{
