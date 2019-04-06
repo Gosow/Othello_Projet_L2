6fb3be3975ebc05965819fbc976d9b705ca60722
Alkassoum_Yacine <yacine.alkassoum.etu@univ-lemans.fr>
2019-04-07 00:48:07 +0200

    nb_possibilites

--- Othello_Projet_L2-master/src/eval.c
+++ Othello_Projet_L2-master/src/eval.c
@@ -3,19 +3,42 @@
 #include "gest_matrice.h"
 //nombre de possibilites du joueur adverse apres que le joueur en cours ai joué un coup
 
-int nb_possibilites(t_matrice mat , int x , int y , char coul )
+int nb_possibilites_adv(t_matrice mat , int x , int y , char coul )
 {
     int nb=0;
     t_matrice temp;
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
-            temp[i][j]=mat[i][j]
+            temp[i][j]=mat[i][j];
 
         }
     jouer_coup(temp,x,y,coul);
-    for(i=0;i<N;i++){
-		for(j=0;j<N;j++){
-			if(coup_valide(mat, coul=="NOIR"?"BLANC":"NOIR", i, j) == 1)
+    for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			if(coup_valide(temp, coul=="NOIR"?"BLANC":"NOIR", i, j) == 1)
+            nb++;
+		}
+	}
+    return nb;
+
+    }
+}
+
+// nb possibilites jouables apres un coup par le joueur coul
+int nb_possibilite(t_matrice mat , int x , int y , char couleur){
+
+
+    int nb=0;
+    t_matrice temp;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            temp[i][j]=mat[i][j];
+
+        }
+    jouer_coup(temp,x,y,couleur);
+    for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			if(coup_valide(temp, couleur, i, j) == 1)
             nb++;
 		}
 	}
