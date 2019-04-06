a67bdfdbbb5c278c7d5bfc351a50f9d845799ca9
Alkassoum_Yacine <yacine.alkassoum.etu@univ-lemans.fr>
2019-04-06 23:05:48 +0200

    restart fonction eval

--- Othello_Projet_L2-master/src/min_max.c
+++ Othello_Projet_L2-master/src/min_max.c
@@ -109,8 +109,8 @@ int alphabeta(t_matrice m, int depth, int alpha, int beta, char noeud)
 
 	if (partie_termineebis(mat) || depth <= 0){
 		//fprintf(stderr,"FINI : ter:%d | prof:%d",partie_termineebis(mat),depth <= 0);
-		if(noeud == MAX) return eval(mat,NOIR);
-		return eval(mat,BLANC);
+		if(noeud == MAX) return fonc_eval(mat,NOIR);
+		return fonc_eval(mat,BLANC);
 	}
 	if(noeud == MAX){ //Programme
 		l = liste_coup(mat,BLANC);
@@ -173,18 +173,27 @@ int alphabeta(t_matrice m, int depth, int alpha, int beta, char noeud)
     }
 }
 
-int fonc_eval(t_matrice mat, int couleur){
+int fonc_eval(t_matrice mat, char couleur){
 
 	int i, j, cpt_final = 0;
 	int haut_G = 0, haut_D = 0, bas_G = 0, bas_D = 0;
-
+	char adv = couleur == NOIR ? BLANC : NOIR;
 		/** La mobilité : chaque case jouable rapporte des pts **/
+
+	if(partie_termineebis(mat)){
+		return (cpt_final=eval(mat,couleur)*100) ;
+	}
+
+
 	for(i=0;i<N;i++){
 		for(j=0;j<N;j++){
 			if(coup_valide(mat, couleur, i, j) == 1)
 				cpt_final += 2;
+			if(coup_valide(mat,adv,i, j) == 1)
+				cpt_final -= 2;
 		}
 	}
+
 
 		/** Le matériel : la différence de pions rapporte des pts **/
 	
@@ -345,231 +354,3 @@ int fonc_eval(t_matrice mat, int couleur){
 }
 
 
-/*
-int main(void)
-{
-	t_matrice m;
-	init_matrice(m);
-    int lig, col;
-	char joueur = BLANC;
-    int choix;
-    int score1=0,score2=0;
-
-	afficher_matrice (m);
-	while (!partie_terminee (m)) {
-		if(joueur == NOIR){
-			choisir_coup (m, &lig, &col, joueur);
-		}else{
-			tour_ordi(m,&lig,&col);
-		}
-		jouer_coup (m, lig, col, joueur);
-		afficher_matrice (m);
-		if (peut_jouer(m, joueur_suivant(joueur))){
-		joueur = joueur_suivant (joueur);
-		}
-		else{ 
-		printf ("\nLe joueur %d passe son tour\n", joueur_suivant(joueur));
-		calculer_score(m,&score1,&score2);
-		printf("il y a %d pions du joueur 1 \n et %d du joueur 2 \n",score1,score2);
-		}
-	}
-}
-*/
-/*
-//fonction qui retourne le nombre de points par rapport a un coup, plus le return est grand et plus le coup est interressant
-int point(t_matrice m , char couleur)
-{	
-	int pt_coins=250;//points des coins(rapportent le plus)
-	int pt_ctr_2=75;//points cotes plt
-	int pt_ctr_1=125;// point pour les cotés du plateau cotes du plateau
-	int pt_centre_plt = 8;//points pour le centre du plateau
-    int i,j;//compteurs ines/jonnes
-    int cpt,calc;//compteurs de points
-    int b_d=0, h_d=0,b_g=0,h_g=0;//haut gauche/droit , bas gauche/droit
-    //chaque case jouable est susceptible de rapporter des points
-	for(i=0;i<N;i++){
-		for(j=0;j<N;j++)
-		{
-			if(coup_valide(m,i,j,couleur))
-			{
-				cpt+=2;
-			}
-
-		}
-	}
-
-	//la difference de nombre entre les pions rapporte aussi des points
-
-		calc+=//fonction qui compte le nombre de pions d une couleur donnée;
-		calc-=//meme fonction avec une operation ternaire ppour prendre l inverse , ou un if
-
-		cpt+= calc;
-
-		//le fait d'avoir certaines cases rapporte plus de points que d autres
-		//exemple les coins
-
-	if(m[N-1][N-1] == couleur){
-		b_d = 1;
-		cpt += pt_coins;
-	}
-	
-	
-	if(m[0][N-1] == couleur){
-		h_d = 1;
-		cpt += pt_coins;
-	}
-	if(m[0][0] == couleur){
-		h_g = 1;
-		cpt += pt_coins;
-	}
-	if(m[N-1][0] == couleur){
-		b_g = 1;
-		cpt += pt_coins;
-	}
-	
-
-	//points des cotés de plateau
-
-	if(h_g != 1){
-		i = 1;
-		j = 1;
-		if(m[i][j] == couleur)
-			cpt -= pt_ctr_1;
-		while(i < N-1){
-			if(m[i][0] == couleur)
-				cpt -= pt_ctr_2;
-			i++;
-		}
-		while(j < N-1){
-			if(m[0][j] == couleur)
-				cpt -= pt_ctr_2;
-			j++;
-		}
-	
-	}
-	else {
-		i = 1;
-		j = 1;
-		while(m[i][0] == couleur && i < N-1){
-			cpt += pt_ctr_1;
-			i++;
-		}
-		while(m[0][j] == couleur && j < N-1){
-			cpt += pt_ctr_1;
-			j++;
-		}
-		
-
-	}
-	if(1 == b_d){
-		i = N-2;
-		j = N-2;
-		while(m[i][N-1] == couleur && i > 0){
-			cpt += pt_ctr_1;
-			i--;
-		}
-		while(m[N-1][j] == couleur && j > 0){
-			cpt += pt_ctr_1;
-			j--;
-		}
-			
-	}
-	else{
-		i = N-2;
-		j = N-2;
-		if(m[i][j] == couleur)
-			cpt -= pt_ctr_1;
-		
-		while(i > 0){
-			if(m[i][N-1] == couleur)
-				cpt += pt_ctr_2;
-			i--;
-		}
-	
-		while(j > 0){
-			if(m[N-1][j] == couleur)
-				cpt -= pt_ctr_2;
-			j--;
-		}
-	}
-	
-	if(1==h_d){
-		j = N-2;
-		i = 1;
-
-		while(m[i][N-1] == couleur && i < N-1){
-			cpt += pt_ctr_1;
-			i++;
-		}
-		while(m[0][j] == couleur && j > 0){
-			cpt += pt_ctr_1;
-			j--;
-		}
-		
-	}
-	else{
-		j = N-2;
-		i = 1;
-		
-		if(m[i][j] == couleur)
-			cpt -= pt_ctr_1;
-		
-		while(i < N-1){
-			if(m[i][N-1] == couleur)
-				cpt -= pt_ctr_2;
-			i++;
-		}
-		while(j > 0){
-			if(m[0][j] == couleur)
-				cpt -= pt_ctr_2;
-			j--;
-		}
-	}
-	if( 1 ==  b_d ){
-		j = 1;
-		i = N-2;
-		
-		while(m[i][0] == couleur && i > 0){
-			cpt += pt_ctr_1;
-			i--;
-		}	
-
-		while(m[N-1][j] == couleur && j < N-1){
-			cpt += pt_ctr_1;
-			j++;
-		}
-	}
-	else{
-		
-		j = 1;
-		i = N-2;
-		if(m[i][j] == couleur)
-			cpt -= pt_ctt_list_coordt_list_coordr_1;
-		
-		while(i > 0){
-			if(m[i][0] == couleur)
-				cpt -= pt_ctr_2;
-			i--;
-		}
-		while(j < N-1){
-			if(m[N-1][j] == couleur)
-				cpt -= pt_ctr_2;
-			j++;
-		}
-	}
-
-		// ET ici le centre du plateau(moins de points)
-
-
-	if(m[N/2][(N/2)-1] == couleur)
-		cpt += pt_centre_plt;
-	if(m[(N/2)-1][N/2] == couleur)
-		cpt += pt_centre_plt;
-	if(m[N/2][N/2] == couleur)
-		cpt += pt_centre_plt;
-	if(m[(N/2)-1][(N/2)-1] == couleur)
-		cpt += pt_centre_plt;
-//retourne le compteur
-	return cpt;
-}
-*/
