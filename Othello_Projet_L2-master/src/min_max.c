afe441fc3de042d575212d329bdb6bd7e8355a8c
Mario <mariohotaj@gmail.com>
2019-04-07 13:59:17 +0200

    Evaluation min_max
    Debug minime à effectuer car il inverse le min et le max entre les 2 joueurs

--- Othello_Projet_L2-master/src/min_max.c
+++ Othello_Projet_L2-master/src/min_max.c
@@ -6,6 +6,206 @@
 
 //gcc min_max.c min_max.h gest_matrice.c liste.c gest_matrice.h define.h
 
+
+
+
+
+
+
+int nb_possibilites_adv(t_matrice mat , int x , int y , char coul )
+{
+    int nb=0;
+    t_matrice temp;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            temp[i][j]=mat[i][j];
+
+        }
+    jouer_coup(temp,x,y,coul);
+    for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			if(coup_valide(temp, coul=="NOIR"?"BLANC":"NOIR", i, j) == 1)
+            nb++;
+		}
+	}
+    return nb;
+
+    }
+}
+
+// nb possibilites jouables apres un coup par le joueur coul
+int nb_possibilite(t_matrice mat , int x , int y , char couleur){
+
+
+    int nb=0;
+    t_matrice temp;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            temp[i][j]=mat[i][j];
+
+        }
+    jouer_coup(temp,x,y,couleur);
+    for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			if(coup_valide(temp, couleur, i, j) == 1)
+            nb++;
+		}
+	}
+    return nb;
+    }
+}
+
+int force(t_matrice m, char couleur){
+    int i, total=0;
+    //Les coins + diagonale
+        total += (m[0][0] == couleur ? 500 : 0);
+        total -= (m[1][1] == couleur ? 250 : 0);
+
+        total += (m[0][N-1] == couleur ? 500 : 0);
+        total -= (m[1][N-2] == couleur ? 250 : 0);
+        
+        total += (m[N-1][0] == couleur ? 500 : 0);
+        total -= (m[N-2][1] == couleur ? 250 : 0);
+        
+        total += (m[N-1][N-1] == couleur ? 500 : 0);
+        total -= (m[N-2][N-2] == couleur ? 250 : 0);
+
+    //coté du coin
+        //coin haut-gauche
+            total -= (m[0][1] == couleur ? 150 : 0);
+            total -= (m[1][0] == couleur ? 150 : 0);
+        //coin haut-droite
+            total -= (m[0][N-1] == couleur ? 150 : 0);
+            total -= (m[N][1] == couleur ? 150 : 0);
+        //coin bas-gauche
+            total -= (m[N-1][0] == couleur ? 150 : 0);
+            total -= (m[1][N] == couleur ? 150 : 0);
+        //coin bas-droite
+            total -= (m[N][N-1] == couleur ? 150 : 0);
+            total -= (m[N-1][N] == couleur ? 150 : 0);
+    
+    for(i=0;i<N-1;i+=N-1){
+        
+        total += (m[i][3] == couleur ? 10 : 0);
+        total += (m[i][4] == couleur ? 10 : 0);
+        total += (m[3][i] == couleur ? 10 : 0);
+        total += (m[4][i] == couleur ? 10 : 0);
+
+        total += (m[i][2] == couleur ? 30 : 0);
+        total += (m[i][5] == couleur ? 30 : 0);
+        total += (m[2][i] == couleur ? 30 : 0);
+        total += (m[5][i] == couleur ? 30 : 0);
+    }
+
+    for(i=2;i<5;i+=3){
+        total += (m[i][3] == couleur ? 2 : 0);
+        total += (m[i][4] == couleur ? 2 : 0);
+        total += (m[3][i] == couleur ? 2 : 0);
+        total += (m[4][i] == couleur ? 2 : 0);
+    }
+
+    total += (m[2][2] == couleur ? 1 : 0);
+    total += (m[2][N-3] == couleur ? 1 : 0);
+    total += (m[N-3][2] == couleur ? 1 : 0);
+    total += (m[N-3][N-3] == couleur ? 1 : 0);
+
+    total += (m[3][3] == couleur ? 16 : 0);
+    total += (m[3][4] == couleur ? 16 : 0);
+    total += (m[4][3] == couleur ? 16 : 0);
+    total += (m[4][4] == couleur ? 16 : 0);
+	return total;
+}
+
+int nb_coup_poss(t_matrice mat ,char coul )
+{
+    int nb=0;
+    t_matrice temp;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            temp[i][j]=mat[i][j];
+
+        }
+    }
+
+    for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			if(coup_valide(temp, coul, i, j) == 1) nb++;
+		}
+	}
+    return nb;
+}
+
+int eval(t_matrice mat, char couleur){
+
+	int i, j, etat;
+	int haut_G = 0, haut_D = 0, bas_G = 0, bas_D = 0;
+	char adv = couleur == NOIR ? BLANC : NOIR;
+    float cpt_final=0, nb_noir, nb_blanc, materiel, nb_coup_adv, nb_coup_jr, pt_force;
+	calculer_score(mat, &nb_noir, &nb_blanc);
+    etat = nb_noir + nb_blanc;
+    
+        /** Le matériel : la différence de pions rapporte des pts **/
+	if(couleur == NOIR) materiel = nb_noir - nb_blanc;
+	else materiel = nb_blanc - nb_noir;
+
+        /** Verifier si la personne a peu de coup possible à faire **/
+    nb_coup_adv = nb_coup_poss(mat,couleur == NOIR ? BLANC : NOIR);
+
+		/** La mobilité : chaque case jouable rapporte des pts **/
+    nb_coup_jr = nb_coup_poss(mat,couleur);
+
+        /** La force : la possession de certaines cases rapporte des pts **/
+    pt_force = force(mat,couleur == NOIR ? BLANC : NOIR) - force(mat,couleur);
+
+
+
+    float avancement = etat/60;
+	
+    if(partie_termineebis(mat)) return materiel*100;
+    
+    cpt_final += pt_force * (1.0-avancement) * (etat <= 20 ? 200 : 1);
+    cpt_final -= nb_coup_adv * (1.0 - avancement);
+    cpt_final += materiel * 200 * avancement;
+	fprintf(stderr,"pt %c: %d",couleur,(int)cpt_final);
+    return (int)cpt_final;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 test DE L'ALPHA BETA
 */
@@ -61,12 +261,11 @@ void tour_ordi(t_matrice m, int* x, int* y){
 	//ec = l;	
 	//t_coord pos;
 	int v, max=-MAX_SCORE;
-	t_matrice temp,mat;
-	copie_mat(m,mat);
+	t_matrice temp;
 	while(!liste_vide(l)){
 		//afficher_matrice(mat);
 		// ICI temp = mat;
-		copie_mat(mat,temp);
+		copie_mat(m,temp);
 		//afficher_matrice(temp);
 		jouer_coup(temp, elem_x(l), elem_y(l), BLANC);
 		
@@ -88,7 +287,7 @@ void tour_ordi(t_matrice m, int* x, int* y){
 	//supprimer_liste(l);
 	//return pos;
 }
-
+/*
 int eval(t_matrice m, char c){
 	int i, j, cmpt=0;
 	for(i=0;i<N;i++){
@@ -99,7 +298,7 @@ int eval(t_matrice m, char c){
 	}
 	return cmpt;
 };
-
+*/
 int alphabeta(t_matrice m, int depth, int alpha, int beta, char noeud)
 {
 	t_liste *l;
@@ -109,8 +308,8 @@ int alphabeta(t_matrice m, int depth, int alpha, int beta, char noeud)
 
 	if (partie_termineebis(mat) || depth <= 0){
 		//fprintf(stderr,"FINI : ter:%d | prof:%d",partie_termineebis(mat),depth <= 0);
-		if(noeud == MAX) return fonc_eval(mat,NOIR);
-		return fonc_eval(mat,BLANC);
+		if(noeud == MAX) return eval(mat,NOIR);
+		return eval(mat,BLANC);
 	}
 	if(noeud == MAX){ //Programme
 		l = liste_coup(mat,BLANC);
